8754339541e49f1005ecad248e43bb3ff4cf00da
Jon <hungnh.uet@gmail.com>
2018-09-06 22:00:13 +0700

    form validation

--- main.js
+++ main.js
@@ -112,6 +112,13 @@ Vue.component('product-review', {
     template: `
         <form class="review-form" @submit.prevent="onSubmit">
           
+            <p v-if="errors && errors.length">
+                <b>Please correct following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
+            
             <p>
                 <label for="name">Name:</label>
                 <input id="name" v-model="name" placeholder="name">
@@ -132,7 +139,7 @@ Vue.component('product-review', {
                   <option>1</option>
                 </select>
             </p>
-              
+
             <p>
                 <input type="submit" value="Submit">  
             </p>
@@ -143,22 +150,29 @@ Vue.component('product-review', {
         return {
             name: null,
             review: null,
-            rating: null
+            rating: null,
+            errors: []
         };
     },
     methods: {
         onSubmit() {
-            let productReview = {
-                name: this.name,
-                review: this.review,
-                rating: this.rating
-            };
+            if (this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                };
 
-            this.$emit('review-submitted', productReview);
+                this.$emit('review-submitted', productReview);
 
-            this.name = null;
-            this.review = null;
-            this.rating = null;
+                this.name = null;
+                this.review = null;
+                this.rating = null;
+            } else {
+                if(!this.name) this.errors.push("Name required.");
+                if(!this.review) this.errors.push("Review required.");
+                if(!this.rating) this.errors.push("Rating required.");
+            }
         }
     }
 });
