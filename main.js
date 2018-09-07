848d5ab4ad01d0f72502e8d21b56c806b0e703e7
Jon <hungnh.uet@gmail.com>
2018-09-07 07:14:33 +0700

    basic example

--- main.js
+++ main.js
@@ -1,43 +1,26 @@
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
-        product: 'Socks',
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: './assets/vmSocks-green.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: './assets/vmSocks-blue.jpg',
-                variantQuantity: 0
-            }
-        ],
-        cart: 0
+        title: 'Hello VueJS',
+        showParagraph: false
     },
     methods: {
-        addToCart() {
-            this.cart += 1;
+        show: function() {
+            this.showParagraph = true;
+            this.updateTitle('Hello VueJS (Updated)');
         },
-        updateProduct(variantIndex) {
-            this.selectedVariant = variantIndex;
+        updateTitle: function(title) {
+            this.title = title;
         }
     },
     computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].variantQuantity > 0;
+        lowercaseTitle: function() {
+            return this.title.toLowerCase();
+        }
+    },
+    watch: {
+        title: function(newValue, oldValue) {
+            alert(`Title changed from "${oldValue}" to "${newValue}"`);
         }
     }
 });
